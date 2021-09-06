5b63cd17c329a8ffd1d349657ca5d90685c01437
Ritaxu0124 <772482292@qq.com>
2021-09-06 17:30:01 +0800

    commit 0906

--- src/DataStructure/sort/RadixSort.java
+++ src/DataStructure/sort/RadixSort.java
@@ -13,6 +13,16 @@ public class RadixSort {
     }
 
     public static void radixSort(int[] arr) {
+        //获取到数组中最大的数
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        //获取最大位数，一共排序maxLength轮
+        int maxLength = (max + "").length();
+
         //定义一个二维数组表示10个桶，每个桶都是一个一维数组
         //1.二维数组包含十个一维数组
         //2.为了防止溢出，每个一维数组的长度为arr.length
@@ -21,6 +31,34 @@ public class RadixSort {
         //为了记录每个桶中存放的数据格式，定义一维数组bucketElementCount来存放每个桶中的数据个数
         int[] bucketElementCount = new int[10];
 
+        for (int i = 0, n = 1; i < maxLength; i++, n *= 10) {
+            //第i轮
+            for (int j = 0; j < arr.length; j++) {
+                //取出每个元素的个位数的值
+                int digitOfElement = arr[j] / n % 10;
+                //放入到对应的桶中
+                bucket[digitOfElement][bucketElementCount[digitOfElement]] = arr[j];
+                bucketElementCount[digitOfElement]++;
+            }
+            //按照桶中数字的顺序依次取出数字，放入到原来的数组中
+            int index = 0;
+            for (int k = 0; k < bucket.length; k++) {
+                //如果桶中有数据，则取出放入原数组
+                if (bucketElementCount[k] != 0) {
+                    //依次将桶中的每个数据放入原数组
+                    for (int l = 0; l < bucketElementCount[k]; l++) {
+                        arr[index++] = bucket[k][l];
+                    }
+                    //取完数据后，需要将bucketElementCount[k]清零
+                    bucketElementCount[k] = 0;
+                }
+            }
+            System.out.println("第" + (i + 1) + "轮排序后的结果：" + Arrays.toString(arr));
+        }
+        System.out.println("####################");
+
+
+        /*
         //第1轮
         for (int j = 0; j < arr.length; j++) {
             //取出每个元素的个位数的值
@@ -70,7 +108,7 @@ public class RadixSort {
         //第3轮
         for (int j = 0; j < arr.length; j++) {
             //取出每个元素的百位数的值
-            int digitOfElement = arr[j] /100 % 10;
+            int digitOfElement = arr[j] / 100 % 10;
             //放入到对应的桶中
             bucket[digitOfElement][bucketElementCount[digitOfElement]] = arr[j];
             bucketElementCount[digitOfElement]++;
@@ -89,6 +127,8 @@ public class RadixSort {
             }
         }
         System.out.println("第3轮排序后的结果：" + Arrays.toString(arr));
+
+         */
     }
 
 
